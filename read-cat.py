d57e1b42d08a3efd05ccc8221568a92f70969da9
Chris <chris.mcnally@pobox.com>
2026-02-06 15:23:20 +0000

    fix some categories, medical

--- read-cat.py
+++ read-cat.py
@@ -1,10 +1,11 @@
+
+
 import read_milen as mil
 import read_chase as chase
 import update_csv_trans as cat
 import gspread 
 import json
 import os
-import csv
 import pandas as pd
 
 PATH_F = "/Users/cmcnally/Dropbox/python/textfiles/"
@@ -121,7 +122,7 @@ def handle_ally(file,account):
     write_to_sheet(dataf)
 
 
-file = "Portuguese_bank-13-Jan-31-Jan.csv"
+file = "Portuguese-bank-feb-2026-partial.csv"
 #handle_millenium(file)
 file = "Chase5869_Activity_20251231.csv"
 #handle_chase(file)
@@ -129,5 +130,6 @@ file = "Schwab_751_Checking_31012026.csv"
 #handle_schwab(file,"Sch-Checking-751")
 #handle_schwab(file,"Sch-192-Rosemary")
 file = "ally_1051307708_2025.csv"
+#handle_ally(file,"ally-1051307708")
 file = "ally_sav-mc-combo-2025.csv"
-handle_ally(file,"ally-2151307713-2190559050")
+#handle_ally(file,"ally-2151307713-2190559050")
